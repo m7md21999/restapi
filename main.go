39669593e32fc60a51621b640174ac1107d9bbb3
Mohammed <m7md21999@gmail.com>
2021-08-15 05:08:49 -0700

    1st commit & add project

---
+++ main.go
@@ -0,0 +1,88 @@
+package main 
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	_ "math/rand"
+	"net/http"
+	_ "strconv"
+)
+
+type Book struct {
+	ID string `json:"id"`
+
+
+	Isbn string `json:"isbn"`
+
+	Title string `json:"title"`
+
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var books []Book
+
+
+func getBooks(w http.ResponseWriter, r *http.Request){
+
+	w.Header().Set("Content-Type" , "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request){
+	
+	w.Header().Set("Content-Type" , "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range books {
+		if item.ID == params["id"] {
+		json.NewEncoder(w).Encode(item)
+		return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request){
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request){
+	
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request){
+	
+}
+
+
+func main()  {
+
+	r := mux.NewRouter()
+
+	books = append (books, Book {ID: "1" , Isbn: "3434343" , Title:"Book one", Author: &Author {Firstname: "Mohammed", Lastname: "Aldawsari"} })
+
+	books = append (books, Book {ID: "2" , Isbn: "32444444444" , Title:"Book Two", Author: &Author {Firstname: "Nasser", Lastname: "Steve"} })
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+
+ 
+
+}
